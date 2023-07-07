da6c464cea26ece180ddca544286b67b5bc06250
Domagoj Šimatović <dosimatovic@gmail.com>
2023-07-07 19:03:10 +0200

    Randomstring vjezba

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  var randomstring = require("randomstring");
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +10,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Moja prva aplikacija</h1>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        {randomstring.generate()}
       </header>
     </div>
   );
